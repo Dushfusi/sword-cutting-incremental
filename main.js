5cd6efcd4cc5e9dec3a8240203be731d1808e39f
Dushfusi <dushfusi@gmail.com>
2025-06-18 14:09:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let intervalDuration = 1000;  // milliseconds (1 second)
 let gameInterval = null;
-var energy = 1600;
+var energy = 0;
 var canClick = true;
 var swords = 0;
 var manualClicksThisSecond = 0;
@@ -240,3 +240,5 @@ updateAttackSpeedDisplay();
 openTab('mainTab');
 // Initialize visuals on page load
 updateSkillVisuals();
+updateEnergyText();
+updateEnergyPerSecond();
